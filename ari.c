dd3eae98e947acf647fdbd39e5ffda9186e962c2
silverline-X <84576570+silverline-X@users.noreply.github.com>
2021-07-01 18:45:27 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,16 +1,16 @@
-#include <string.h>
-#include <cs50.h>
 string ari(string S)
 {
 int words=0;int chara=0;int sent=0;
 for(int i=0;S[i]!='\0';i++)
   {
-   if(isalnum(S[i])==1){chara++;}
-   else if(S[i]=='.'||S[i]=='?'||S[i]=='!'){sent++;}
-   else if(S[i]==' '){words++;}
+   if((isalnum(S[i]))!=0){chara++;}
+   if(S[i]=='.'||S[i]=='?'||S[i]=='!'){sent++;}
+   if(S[i]==' '){words++;}
   }
-float score=4.71*((float)chara/words)+0.5*((float)words/sent)-21.43;
-score=(int)score+1;
+float ratio1=4.71*(float)chara/words;
+float ratio2=0.5*(float)words/sent;
+float score=ratio1+ratio2-21.43;
+int Score_Index=(int)score+1;
 string out[]={"Kindergarten","First/Second Grade","Third Grade","Fourth Grade","Fifth Grade","Sixth Grade","Seventh Grade","Eighth Grade","Ninth Grade","Tenth Grade","Eleventh Grade","Twelfth grade","College student","Professor"};
-return out[score-1];
+return out[Score_Index-1];
 }
